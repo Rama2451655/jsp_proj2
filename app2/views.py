b736f48b89f09078b948acf2685f80b9b5f75460
Rama2451655 <levakuramakrishnareddy4d2@gmail.com>
2023-11-08 16:24:34 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.Http import HttpResponse
+def silk(request):
+  return HttpResponse("<marquee><h1>My Name is Ramakrishna .Iam form kadapa corrently pursing in jsp.</marquee><h1>")
+
+# Create your views here.
